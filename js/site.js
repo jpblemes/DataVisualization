4b1662fa949d0702de484cf9421c13c8bc0fa5d2
João Paulo Borges Lemes <jpblemes@hotmail.com>
2019-06-08 21:09:17 -0300

    a

--- js/site.js
+++ js/site.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $('.v1').tooltip();
-});
-
-$(document).ready(function(){
-    $('.v2').tooltip();
-});
-      
-$(document).ready(function(){
-    $('.v3').tooltip();
-});
-      
-=======
 $(document).ready(function(){
     $('.toolTipOn').tooltip();
 });
 
 
       
->>>>>>> master
